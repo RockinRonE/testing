2784d1abd87a84360d14c582a2bc53bd82d56f77
Ronny Ewanek <ronny.ewanek@gmail.com>
2017-09-01 13:52:13 -0500

    finished testing

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-export default class CommentBox extends Component {
+class CommentBox extends Component {
     state = { comment: '' };
 
     handleChange = (event) => {
@@ -9,19 +11,25 @@ export default class CommentBox extends Component {
 
     handleSubmit = (event) => {
         event.preventDefault();
-
+        this.props.saveComment(this.state.comment);
         this.setState({ comment: '' });
     }
     
     render() {
         return(
             <form onSubmit={this.handleSubmit} className="comment-box">
+                <h4>Add a comment</h4>
                 <textarea 
                     value={this.state.comment} 
                     onChange={this.handleChange} 
                 />
-                <button action="submit"> Submit Comment</button>
+                <div>
+                    <button action="submit"> Submit Comment</button>
+                </div>
             </form>
         );
     }
 }
+
+
+export default connect(null, actions )(CommentBox);
--- test/test_helper.js
+++ test/test_helper.js
@@ -1,36 +1,43 @@
+import jsdom from 'jsdom';
 import _$ from 'jquery';
+import TestUtils from 'react-addons-test-utils';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TestUtils from 'react-addons-test-utils';
-import jsdom from 'jsdom';
 import chai, { expect } from 'chai';
-import chaiJquery from 'chai-jquery';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from '../src/reducers';
+import chaiJquery from 'chai-jquery';
 
+// Set up testing environment like a browser in the command line
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView;
-global.navigator = global.window.navigator;
-const $ = _$(window);
+    // assign jquery to this, not default DOM
+const $ = _$(global.window);
 
-chaiJquery(chai, chai.util, $);
+// Build 'renderComponent' helper that should render a given react class
+function renderComponent(ComponentClass, props, state) {
+    const componentInstance = TestUtils.renderIntoDocument(
+        <Provider store={createStore(reducers, state)} >
+            <ComponentClass {...props} />
+        </Provider>
+    );
 
-function renderComponent(ComponentClass, props = {}, state = {}) {
-  const componentInstance =  TestUtils.renderIntoDocument(
-    <Provider store={createStore(reducers, state)}>
-      <ComponentClass {...props} />
-    </Provider>
-  );
-
-  return $(ReactDOM.findDOMNode(componentInstance));
+    return $(ReactDOM.findDOMNode(componentInstance)); // produced HTML
 }
 
+// Build helper for simulating events
 $.fn.simulate = function(eventName, value) {
-  if (value) {
-    this.val(value);
-  }
-  TestUtils.Simulate[eventName](this[0]);
-};
+    if (value) {
+        this.val(value);
+    }
+    TestUtils.Simulate[eventName](this[0]);
+}
 
-export {renderComponent, expect};
+// Set up chai jquery
+chaiJquery(chai, chai.util, $);
+
+
+
+
+export { renderComponent, expect };
